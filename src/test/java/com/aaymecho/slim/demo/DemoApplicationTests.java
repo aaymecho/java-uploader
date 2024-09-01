e8e327076db98bec4e0d2882960ac36067ea7382
slimzshay <slimmylsrp@gmail.com>
2024-09-01 12:44:51 -0400

    created FileInfo model

---
+++ src/test/java/com/aaymecho/slim/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaymecho.slim.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
